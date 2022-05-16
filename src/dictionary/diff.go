75f3768245a750e1fb7ce87e395126932d77fc3d
Junghoon Noh <jungnoh@maasasia.com>
2022-05-16 12:26:54 +0900

    add: ContentRepresentation diff

---
+++ src/dictionary/diff.go
@@ -0,0 +1,66 @@
+package dictionary
+
+type DiffFlag string
+
+const (
+	CreateFlag DiffFlag = "CREATE"
+	DeleteFlag DiffFlag = "DELETE"
+	ChangeFlag DiffFlag = "CHANGE"
+)
+
+type ContentDifference struct {
+	CreatedKeys []EntryKey
+	DeletedKeys []EntryKey
+	Changes     map[EntryKey]map[string]DiffFlag
+}
+
+func DiffContents(from, to ContentRepresentation) ContentDifference {
+	diff := ContentDifference{
+		CreatedKeys: []EntryKey{},
+		DeletedKeys: []EntryKey{},
+		Changes:     map[EntryKey]map[string]DiffFlag{},
+	}
+
+	flatFrom, flatTo := from.ToFlattened(), to.ToFlattened()
+	for key := range *flatFrom {
+		if _, ok := (*flatFrom)[key]; !ok {
+			diff.DeletedKeys = append(diff.DeletedKeys, key)
+		}
+	}
+	for key := range *flatTo {
+		if _, ok := (*flatTo)[key]; !ok {
+			diff.CreatedKeys = append(diff.CreatedKeys, key)
+		} else {
+			if len((*flatTo)[key]) == 0 && len((*flatFrom)[key]) == 0 {
+				continue
+			}
+			if len((*flatTo)[key]) == 0 {
+				diff.DeletedKeys = append(diff.DeletedKeys, key)
+				continue
+			}
+			if len((*flatFrom)[key]) == 0 {
+				diff.CreatedKeys = append(diff.CreatedKeys, key)
+				continue
+			}
+
+			diff.Changes[key] = map[string]DiffFlag{}
+			for lang := range (*flatFrom)[key] {
+				if _, ok := (*flatTo)[key][lang]; !ok {
+					diff.Changes[key][lang] = DeleteFlag
+				}
+			}
+			for lang, langValue := range (*flatTo)[key] {
+				if langFromValue, ok := (*flatFrom)[key][lang]; !ok {
+					diff.Changes[key][lang] = CreateFlag
+				} else if langValue != langFromValue {
+					diff.Changes[key][lang] = ChangeFlag
+				}
+			}
+			if len(diff.Changes[key]) == 0 {
+				delete(diff.Changes, key)
+			}
+		}
+	}
+
+	return diff
+}
